c6010ecda82018f022c7b0c62b8a9aae71037afc
seli <selisinha22@gmail.com>
2022-02-04 15:52:38 +0530

    first commit

---
+++ src/myComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const About = () => {
+    let myStyle2={
+        minHeight: "70vh",
+        margin:"40px auto"
+      }
+  return (
+  <div className="container" style={myStyle2}>
+      <h3>About</h3>
+      <p>This is a simple todo app made with html, css, javascript and react.</p>
+  </div>);
+};
---
+++ src/myComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Footer = () => {
+  // let footerStyle={
+  //   position: "relative",
+  //   top: "70vh",
+  //   width: "100%"
+  // }
+  return( 
+  <footer className="bg-primary text-center text-light py-3" >
+     <p>
+       Copyright &copy; MyToDoList.com
+     </p>
+  </footer>
+  )
+};
+
